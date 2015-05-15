d79448541d8d8adc78da2e1aa95f5e2de5c22f71
carlos.silva <carlos.silva@7041b7d6-f801-4286-9b3d-420f2f459141>
2015-05-15 14:21:04 +0000

    CRUD Layer(MAPS)

--- app/maps/layer/controller/layer-form.js
+++ app/maps/layer/controller/layer-form.js
@@ -9,7 +9,7 @@ define([
         $scope.layerSource = null;
 
 
-        LayerSourceService.list().then(function (response) {            
+        LayerSourceService.list().then(function (response) {
             $scope.layerSourceType = response.data;
         }, function (response) {
             console.info("error", response);
@@ -20,17 +20,39 @@ define([
             $scope.isEditing = true;
             LayerService.get($routeParams.id).success(function (data) {
                 $scope.layer = data;
+                $scope.getLayersBySource($scope.layer.layerSourceEntity.id);
+                                               
+                var interval = setInterval(function () {
+                    if ($scope.formulario.ds_layer.$viewValue !== "") {
+                        angular.element("#layerSourceEntityID").find("option[label='" + $scope.layer.layerSourceEntity.nm_source + "']").attr('selected', 'true');
+                        clearInterval(interval);
+                    }
+
+                });
+
             });
         }
 
-        $scope.getLayersBySource = function (idLayerSource) {                 
+        $scope.getLayersBySource = function (idLayerSource) {
             LayerService.getLayersBySource(idLayerSource, $scope);
             LayerSourceService.get(idLayerSource).success(function (response) {
                 $scope.layerSource = response;
+
+                if ($scope.isEditing) {
+                    
+                    var interval=setInterval(function(){
+                        if(typeof angular.element("#nm_layer").find("option[label='" + $scope.layer.nm_layer + "']").val() !== 'undefined'){
+                            var layerNameValue = angular.element("#nm_layer").find("option[label='" + $scope.layer.nm_layer + "']").val();
+                            angular.element("#nm_layer").val(layerNameValue);                        
+                            clearInterval(interval);
+                        }                        
+                    }); 
+                }
+
             });
         };
 
-        $scope.getLayerConfigs = function (layerName) {            
+        $scope.getLayerConfigs = function (layerName) {
             LayerService.getLayerColumns($scope.layerSource.ds_link_interno, layerName, $scope);
             LayerService.getLayerGeometryType($scope.layerSource.ds_link_interno, layerName, $scope);
         };
