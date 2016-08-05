bd52cb6bec6a7c22a4d7840684e2f04054347b8c
Vinicius da Costa Pires <vinicius.costa.pires@gmail.com>
2016-08-05 17:50:36 +0000

    Corrigindo problema de performance nos listeners de click-out

--- app/portal/layout/directive/click-out.js
+++ app/portal/layout/directive/click-out.js
@@ -1,38 +1,36 @@
 /* global angular */
 define([
-    'connecta.portal'
-], function (portal) {
-    return portal.lazy.directive('clickOut', function ($window, $timeout) {
+    'connecta.portal',
+    'portal/layout/service/click-out',
+    'portal/layout/service/util'
+], function (portal, clickout) {
+    return portal.lazy.directive('clickOut', function ($clickOutManager, util, $timeout) {
         return {
             restrict: 'A',
             scope: {
                 clickOut: '&',
                 clickOutExceptions: '='
             },
-            link: function (scope, element) {
-                var _element = element[0];
-                
-                function _getElements(){
-                    var _elements = [];
-                    
-                    if (scope.clickOutExceptions) {
-                        scope.clickOutExceptions.forEach(function(exception){
-                            _elements.push(angular.element(exception).get(0));
-                        });
-                    }
-                    
-                    return _elements;
-                }
-                
-                angular.element($window).on('click', function (event) {
-                    var els = _getElements().filter(function(el){
-                        return el && el.contains(event.target);
-                    });
-                    if (!_element.contains(event.target) && els.length === 0) {
-                        $timeout(function(){
-                            scope.clickOut();
-                        });
-                    }
+            controller:function($scope, $element){
+		$scope.__uuid = util.uuid();
+
+		var _clickOut = function(){
+	           $timeout(function(){
+                       $scope.clickOut();
+                   });
+                };
+
+		console.debug($scope.clickOutExceptions);
+
+		$clickOutManager.addListener($scope.__uuid, {
+                    exceptions: $scope.clickOutExceptions,
+                    scope: $scope,
+	            element: $element,
+                    onClickOut:_clickOut
+		});
+
+                $element.on('$destroy', function(){
+                    $clickOutManager.removeListener($scope.__uuid);
                 });
             }
         };
