cb92372ed0fce69b003b5ad8e4cd0af03fb567e8
ulisses.carvalho <ulisses.carvalho@7041b7d6-f801-4286-9b3d-420f2f459141>
2016-09-12 17:10:39 +0000

    List spatial datasource

--- app/maps/spatial-datasource/controller/spatial-datasource-list.js
+++ app/maps/spatial-datasource/controller/spatial-datasource-list.js
@@ -3,16 +3,16 @@ define([
   "maps/spatial-datasource/service/spatial-datasource-service"
 ], function (maps) {
 
-  return maps.lazy.controller("SpatialDataSourceListController", function ($scope, SpatialDataSourceService, $location) {
+  return maps.lazy.controller("SpatialDataSourceListController", function ($scope, SpatialDataSourceService, $location, notify) {
 
     $scope.spatialDataSources = {};
 
-    SpatialDataSourceService.list().success(function (response) {
-      $scope.spatialDataSources = response.data;
-      console.info("OK ==>", response);
-    }).error(function (error) {
-      console.info("Erro ==>", error);
+    SpatialDataSourceService.list().then(function (response) {
+      $scope.spatialDataSources = response.data.data;
+    }, function (error) {
+      notify.error(error);
     });
+
 
     $scope.openView = function (id) {
       $location.path("/maps/spatial-datasource/" + id + "/view");
